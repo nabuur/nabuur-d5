c48f61ffcaf75e1798d4f8dd4e5d8b3c4b299379
frans <frans@735dea64-16db-426a-a501-683fc2a7293f>
2009-08-03 13:56:44 +0000

    upgrade imagefiled to rev DRUPAL-5--2-6
    git-svn-id: file:///home/frans/tmp/svn-to-git/nabuur-svn/trunk@559 735dea64-16db-426a-a501-683fc2a7293f

--- sites/all/modules/imagefield/imagefield.js
+++ sites/all/modules/imagefield/imagefield.js
@@ -1,32 +1,28 @@
-// $Id: imagefield.js,v 1.1.2.2 2008/08/01 18:02:16 dopry Exp $
+// $Id: imagefield.js,v 1.1.2.4 2009/03/15 00:25:29 quicksketch Exp $
 
 /**
  *  Auto Attach standard client side file input validation
  */
 Drupal.imagefieldValidateAutoAttach = function() {
-  $("input[@type='file'].imagefield").change( function() {
+  $("input.imagefield[@type='file']").change(function() {
+    // Remove any previous errors.
     $('.imagefield-js-error').remove();
-    /**
-     *  add client side validation for the input[@file] accept attribute
-     */
- 
-    if(this.accept.length>1){
-      accept = this.accept.replace(',','|');
-      v = new RegExp('\\.('+(accept?accept:'')+')$','gi');
-      if (!v.test(this.value)) {
-        var error = 'The file ' + this.value + " is not supported.\n";
-        error += "Only the following file types are supported: \n" + accept.replace(/\|/g, ', ');
-        alert(error);
-        // what do I prepend this to? 
-        //$(this).prepend($('<div class="imagefield-js-error>"' + error + '</div>'));
-        this.value = ''; 
-        return false;
-      }   
-    }   
-    /**
-     * Add filesize validation where possible
-     */
 
+    /**
+     * Add client side validation for the input[@file] accept attribute
+     */
+    var accept = this.accept.replace(/,\s*/g, '|');
+    if (accept.length > 1) {
+      var v = new RegExp('\\.(' + accept + ')$', 'gi');
+      if (!v.test(this.value)) {
+        var error = 'The selected file ' + this.value + ' cannot not be uploaded. Only files with the following extensions are allowed: ' + accept.replace(/\|/g, ', ');
+
+        $(this).before('<div class="messages error file-upload-js-error">' + error + '</div>');
+        this.value = '';
+
+        return false;
+      }
+    }
   }); 
 }
 
